7791fd16dc634f1065d5914f9d7e60888ec7103e
Joseph Werle <joseph.werle@gmail.com>
2018-06-02 18:09:31 -0400

    refactor(index.js): Export normalize function

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const { Authentication } = require('./authentication')
 const { DIDDocument } = require('./document')
 const { PublicKey } = require('./public-key')
+const { normalize } = require('./normalize')
 const { Service } = require('./service')
 
 module.exports = {
   Authentication,
   DIDDocument,
   PublicKey,
+  normalize,
   Service,
 }
